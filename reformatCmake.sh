1cbf2ad8f5ff9e830f62a53d44c2f9e6ece4f8a2
Claude Mally <claude.mally@gmail.com>
2024-08-23 16:30:21 -0400

    uoodated reformatCmake.sh

--- reformatCmake.sh
+++ reformatCmake.sh
@@ -2,5 +2,5 @@
 
 # Find all CMake files and format them
 find . -name "CMakeLists.txt" -o -name "*.cmake" | while read -r file; do
-    cmake-format -i "$file"
+    cmake-format -i -c .cmake-format.yaml "$file"
 done
