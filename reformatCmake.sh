df4f9e758769f48e012cf7305f0e7dca6c8357fc
Claude Mally <claude.mally@gmail.com>
2024-08-25 11:29:43 -0400

    Removed unused / broked shell scripts

--- reformatCmake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Find all CMake files and format them
-find . -name "CMakeLists.txt" -o -name "*.cmake" | while read -r file; do
-    cmake-format -i -c .cmake-format.yaml "$file"
-done
