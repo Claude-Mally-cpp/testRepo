f1c7a1004b6109e218080accd425e670e8711980
Claude Mally <claude.mally@gmail.com>
2024-09-01 09:38:47 -0400

    changed dice so that distribution is optional

--- include/hobby/hobbyDice.h
+++ include/hobby/hobbyDice.h
@@ -1,4 +1,5 @@
 #include <concepts>
+#include <optional>
 #include <random>
 #include <stdexcept>
 
@@ -8,8 +9,7 @@ template <std::unsigned_integral T = uint32_t>
 class dice {
    public:
     // Constructor that accepts the number of sides as a runtime parameter
-    explicit dice(T sides)
-        : generator(randomDevice()), distribution(1, sides), sides(sides) {
+    explicit dice(T sides) : sides(sides), generator(randomDevice()) {
         if (sides == 0) {
             throw std::invalid_argument(
                 "Number of sides must be greater than zero");
@@ -17,7 +17,12 @@ class dice {
     }
 
     // Method to roll the dice and get a random value between 1 and sides
-    T roll() { return distribution(generator); }
+    T roll() {
+        if (!distribution.has_value()) {
+            distribution.emplace(1, sides);
+        }
+        return (*distribution)(generator);
+    }
 
     // Getter for the number of sides
     T getSides() const noexcept { return sides; }
@@ -25,7 +30,7 @@ class dice {
    private:
     std::random_device randomDevice;
     std::mt19937 generator;
-    std::uniform_int_distribution<T> distribution;
+    std::optional<std::uniform_int_distribution<T>> distribution;
     T sides;
 };
 
