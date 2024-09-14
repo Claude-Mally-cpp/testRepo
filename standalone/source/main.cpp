8dfaed1c64da1338bd68797349c78f39035ef63f
Claude Mally <claude.mally@gmail.com>
2024-09-14 16:26:51 -0400

    window option

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -2,15 +2,16 @@
 #include <fmt/ranges.h>
 #include <hobby/version.h>
 
+#include <SFML/Window.hpp>
 #include <cassert>
 #include <cxxopts.hpp>
 #include <hobby/hobby.hpp>
 #include <hobby/hobbyDice.hpp>
 #include <hobby/hobbyMath.hpp>
-// #include <iostream>
-// #include <ranges>
 #include <string>
 #include <unordered_map>
+
+void playWithWindow();
 
 auto main(int argc, char** argv) -> int {
     try {
@@ -36,6 +37,7 @@ auto main(int argc, char** argv) -> int {
     ("p,prime", "test if prime", cxxopts::value<int64_t>())
     ("r,random", "throw a N sided dice", cxxopts::value<uint64_t>())
     ("g,generate", "generate an array of 10 primes higher that argument", cxxopts::value<uint64_t>())
+    ("w,window", "Open an SFML window")
   ;
         // clang-format on
 
@@ -48,6 +50,11 @@ auto main(int argc, char** argv) -> int {
 
         if (result["version"].as<bool>()) {
             fmt::println("Hobby, version {}", HOBBY_VERSION);
+            return 0;
+        }
+
+        if (result["window"].as<bool>()) {
+            playWithWindow();
             return 0;
         }
 
@@ -109,4 +116,21 @@ auto main(int argc, char** argv) -> int {
         fmt::println("caught exception {}", except.what());
     }
     return 0;
+}
+
+void playWithWindow() {
+    auto title = fmt::format("Hobby window version {}", HOBBY_VERSION);
+    sf::Window window(sf::VideoMode(800, 600), title);
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last
+        // iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
 }
